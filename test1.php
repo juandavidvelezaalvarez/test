aa6c0f7eb7bdfd9f39f26ffe040b565028f3a51d
Juan David Velez Alvarez <juan.velez@and.gov.co>
2022-07-01 09:21:38 -0500

    second

--- test1.php
+++ test1.php
@@ -1,3 +1,6 @@
 <?php
-echo "hello world";
+error_reporting(E_ALL);
+$url = 'https://servicios.laequidadseguros.coop/cotizador/ServletCodigoFasecoldaPlaca?placa=bru268';
+$homepage = file_get_contents($url);
+echo $homepage;
 ?>
